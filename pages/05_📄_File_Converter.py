5ec10210c0edb9db52a3c9b582dc7b42283a0ca0
Marcial Bernal <marcialb9328@gmail.com>
2025-12-10 18:47:04 -0700

    corregido servicio de file converter

--- pages/05_📄_File_Converter.py
+++ pages/05_📄_File_Converter.py
@@ -2,52 +2,37 @@ import streamlit as st
 import pandas as pd
 import io
 
-
 @st.cache_data
 def file_converter(file):
-    # Detectar extensión
-    if file.name.endswith(".xlsx"):
-        excel_file = pd.read_excel(file, engine="openpyxl")
-    else:
-        excel_file = pd.read_excel(file)
-    
-    csv_file = excel_file.to_csv(index=False).encode('utf-8')
-    return csv_file
-
+    df = pd.read_excel(file, engine="openpyxl")
+    return df.to_csv(index=False).encode("utf-8")
 
 st.set_page_config(
-    page_title=" Excel Converter",
+    page_title="Excel Converter",
     page_icon="📄",
-    )
+)
 
 st.title("📄 Excel to CSV Converter")
 
-uploaded_file = st.file_uploader("Upload an Excel file", type=["xls", "xlsx"])
+uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
 
 if uploaded_file:
-    try:
-        st.success(f"File '{uploaded_file.name}' uploaded successfully!")
-        
-    except:
-        st.error("An error ocurred, maybe the file type its not correct")
-        
+    st.success(f"File '{uploaded_file.name}' uploaded successfully!")
 
-    
     if st.button("Convert to CSV"):
-        
         try:
             csv = file_converter(uploaded_file)
-            file_preview = pd.read_csv(io.BytesIO(csv))
-            st.write("✅ File Preview:")
-            st.dataframe(file_preview.head())
+            preview = pd.read_csv(io.BytesIO(csv))
 
-            
+            st.write("✅ File Preview:")
+            st.dataframe(preview.head())
+
             st.download_button(
                 label="⬇️ Download CSV file",
                 data=csv,
-                file_name=uploaded_file.name.replace(".xlsx", ".csv").replace(".xls", ".csv"),
+                file_name=uploaded_file.name.replace(".xlsx", ".csv"),
                 mime="text/csv"
             )
-            
-        except:
-            st.error("An error has ocurred")
+        except Exception as e:
+            st.error("An error has occurred while converting the file.")
+            st.exception(e)
