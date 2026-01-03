bbc3eb118ddb6f6fa91a42803b30d5d43adc099d
Marcial Bernal <marcialb9328@gmail.com>
2026-01-02 18:54:09 -0700

    Creado about project del scraper

--- pages/03_👜_MercadoLibre_Scraper.py
+++ pages/03_👜_MercadoLibre_Scraper.py
@@ -3,29 +3,54 @@ import pandas as pd
 import os
 from services.web_scraper import run_scraper
 
-st.title("WEB SCRAPPER  e_comerce - Mercado Libre")
+st.set_page_config(layout="wide")
+st.title('''WEB SCRAPPER Mercado Libre''')
 
-search_query = st.text_input("Input an item to search on e_commerce - Mercado Libre:")
+col1, col2 = st.columns([3,4])
 
-if search_query:
-    st.write(f"Search results of item: {search_query}")
+with col1:
+    search_query = st.text_input("Input an item to search on Mercado Libre:")
 
-    df, file_name = run_scraper(search_query)
+    if search_query:
+        st.write(f"Search results of item: {search_query}")
 
-    if not df.empty:
-        st.dataframe(df)
+        df, file_name = run_scraper(search_query)
 
-        with open(file_name, "rb") as f:
-            st.download_button(
-                label="⬇️ Download Excel",
-                data=f,
-                file_name="search_results.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
+        if not df.empty:
+            st.dataframe(df, use_container_width=True)
 
-        try:
-            os.remove(file_name)
-        except FileNotFoundError:
-            pass
-    else:
-        st.error("No items found")
+            with open(file_name, "rb") as f:
+                st.download_button(
+                    label="⬇️ Download Excel",
+                    data=f,
+                    file_name="search_results.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
+
+            try:
+                os.remove(file_name)
+            except FileNotFoundError:
+                pass
+        else:
+            st.error("No items found")
+
+
+with col2:
+    st.markdown(
+        """
+        ### About this project
+        A Python-based web scraping tool that extracts product data from MercadoLibre México using a search query.
+
+        The scraper retrieves product titles, prices, images, URLs, and fulfillment status, structures the data with Pandas, and exports the results to an Excel file.
+
+        Built with a modular design and clean data handling, this project demonstrates practical experience with HTTP requests, HTML parsing, and data processing, and can be easily integrated into applications such as Streamlit dashboards.
+
+        Tech stack: \n
+        - Python \n
+        - Requests \n
+        - BeautifulSoup \n
+        - Pandas
+        """
+        )
+    st.divider()
+    st.video("https://www.youtube.com/watch?v=sF80I-TQiW0")
